2a84a236984a2c465709af5c34d50db3574a3086
ali ouchagour <ali.ouchagour01@gmail.com>
2023-03-27 23:46:04 +0000

    removing innececry  views and complete dashboard app layout

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,48 @@
+ <!--**********************************
+            Sidebar start
+        ***********************************-->
+        <div class="dlabnav">
+            <div class="dlabnav-scroll">
+				<ul class="metismenu" id="menu">
+                 
+					<li><a href="widget-basic.html" class="" aria-expanded="false">
+					<i class="fa-fw fas fa-cogs nav-icon"></i>
+						<span class="nav-text">Assets</span>
+					</a>
+				</li>
+				<li><a href="widget-basic.html" class="" aria-expanded="false">
+				<i class="fa-fw fas fa-cogs nav-icon"></i>
+						<span class="nav-text">Stocks</span>
+					</a>
+				</li>
+				<li><a href="widget-basic.html" class="" aria-expanded="false">
+						<i class="fa-fw fas fa-cogs nav-icon"></i>
+						<span class="nav-text">Transactions</span>
+					</a>
+				</li>
+				<li><a href="widget-basic.html" class="" aria-expanded="false">
+					<i class="fa fa-solid fa-hospital"></i>
+						<span class="nav-text">Hospitals</span>
+					</a>
+				</li>
+				<li><a class="has-arrow " href="javascript:void()" aria-expanded="false">
+					<i class="fas fa-user-check"></i>
+					<span class="nav-text">User Management</span>
+				</a>
+				<ul aria-expanded="false">
+					<li><a href="index.html">Roles</a></li>
+					<li><a href="index-2.html">Permissions</a></li>
+					<li><a href="project-page.html">liste of users </a></li>
+				
+				</ul>
+
+		
+
+           
+                </ul>
+			
+			</div>
+        </div>
+        <!--**********************************
+            Sidebar end
+        ***********************************-->
