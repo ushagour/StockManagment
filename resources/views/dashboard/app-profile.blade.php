656b3810d8c90e69b18c51f4a3271878a53d755e
ali ouchagour <ali.ouchagour01@gmail.com>
2023-04-01 03:01:25 +0000

    addapting the template and restore auth controllers create asset and stock controllers

---
+++ resources/views/dashboard/app-profile.blade.php
@@ -0,0 +1,196 @@
+﻿@extends('layouts.app')
+
+@section('content')
+		
+
+        <!--**********************************
+            Content body start
+        ***********************************-->
+        <div class="content-body">
+            <div class="container-fluid">
+				
+				
+				<div class="row page-titles">
+					<ol class="breadcrumb">
+						<li class="breadcrumb-item active"><a href="javascript:void(0)">App</a></li>
+						<li class="breadcrumb-item"><a href="javascript:void(0)">Profile</a></li>
+					</ol>
+                </div>
+                <!-- row -->
+           
+                <div class="row">
+                    <div class="col-xl-4">
+						<div class="row">
+							<div class="col-xl-12">
+								<div class="card">
+									<div class="card-body">
+										<div class="profile-statistics">
+											<div class="text-center">
+												<div class="row">
+													<div class="col">
+														<h3 class="m-b-0">150</h3><span>Follower</span>
+													</div>
+													<div class="col">
+														<h3 class="m-b-0">140</h3><span>Place Stay</span>
+													</div>
+													<div class="col">
+														<h3 class="m-b-0">45</h3><span>Reviews</span>
+													</div>
+												</div>
+												<div class="mt-4">
+													<a href="javascript:void(0);" class="btn btn-primary mb-1 me-1">Follow</a> 
+													<a href="javascript:void(0);" class="btn btn-primary mb-1" data-bs-toggle="modal" data-bs-target="#sendMessageModal">Send Message</a>
+												</div>
+											</div>
+											<!-- Modal -->
+											<div class="modal fade" id="sendMessageModal">
+												<div class="modal-dialog modal-dialog-centered" role="document">
+													<div class="modal-content">
+														<div class="modal-header">
+															<h5 class="modal-title">Send Message</h5>
+															<button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+														</div>
+														<div class="modal-body">
+															<form class="comment-form">
+																<div class="row"> 
+																	<div class="col-lg-6">
+																		<div class="mb-3">
+																			<label class="text-black font-w600 form-label">Name <span class="required">*</span></label>
+																			<input type="text" class="form-control" value="Author" name="Author" placeholder="Author">
+																		</div>
+																	</div>
+																	<div class="col-lg-6">
+																		<div class="mb-3">
+																			<label class="text-black font-w600 form-label">Email <span class="required">*</span></label>
+																			<input type="text" class="form-control" value="Email" placeholder="Email" name="Email">
+																		</div>
+																	</div>
+																	<div class="col-lg-12">
+																		<div class="mb-3">
+																			<label class="text-black font-w600 form-label">Comment</label>
+																			<textarea rows="8" class="form-control" name="comment" placeholder="Comment"></textarea>
+																		</div>
+																	</div>
+																	<div class="col-lg-12">
+																		<div class="mb-3 mb-0">
+																			<input type="submit" value="Post Comment" class="submit btn btn-primary" name="submit">
+																		</div>
+																	</div>
+																</div>
+															</form>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						
+						
+							<div class="col-xl-12">
+								<div class="card">
+									<div class="card-body">
+										<div class="profile-news">
+											<h5 class="text-primary d-inline">Our Latest News</h5>
+											<div class="media pt-3 pb-3">
+												<img src="images/profile/5.jpg" alt="image" class="me-3 rounded" width="75">
+												<div class="media-body">
+													<h5 class="m-b-5"><a href="post-details.html" class="text-black">Collection of textile samples</a></h5>
+													<p class="mb-0">I shared this on my fb wall a few months back, and I thought.</p>
+												</div>
+											</div>
+											<div class="media pt-3 pb-3">
+												<img src="images/profile/6.jpg" alt="image" class="me-3 rounded" width="75">
+												<div class="media-body">
+													<h5 class="m-b-5"><a href="post-details.html" class="text-black">Collection of textile samples</a></h5>
+													<p class="mb-0">I shared this on my fb wall a few months back, and I thought.</p>
+												</div>
+											</div>
+											<div class="media pt-3 pb-3">
+												<img src="images/profile/7.jpg" alt="image" class="me-3 rounded" width="75">
+												<div class="media-body">
+													<h5 class="m-b-5"><a href="post-details.html" class="text-black">Collection of textile samples</a></h5>
+													<p class="mb-0">I shared this on my fb wall a few months back, and I thought.</p>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+                    </div>
+                    <div class="col-xl-8">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="profile-tab">
+                                    <div class="custom-tab-1">
+                                    
+                                        <div class="tab-content">
+                                        
+                                            <div>
+                                                <div class="pt-3">
+                                                    <div class="settings-form">
+                                                        <h4 class="text-primary">Account Setting</h4>
+                                                        <form>
+                                                            <div class="row">
+                                                                <div class="mb-3 col-md-6">
+                                                                    <label class="form-label">Email</label>
+                                                                    <input type="email" placeholder="Email" value="{{$user->email}}" class="form-control">
+                                                                </div>
+                                                                <div class="mb-3 col-md-6">
+                                                                    <label class="form-label">name</label>
+                                                                    <input type="text"   value="{{$user->name}}"  class="form-control">
+                                                                </div>
+                                                            </div>
+                                                         
+                                                            <div class="mb-3">
+                                                                <label class="form-label">Address 2</label>
+                                                                <input type="text" placeholder="Apartment, studio, or floor" class="form-control">
+                                                            </div>
+                                                            <div class="row">
+                                                                <div class="mb-3 col-md-6">
+                                                                    <label class="form-label">City</label>
+                                                                    <input type="text" class="form-control">
+                                                                </div>
+                                                                <div class="mb-3 col-md-4">
+                                                                    <label class="form-label">State</label>
+                                                                    <select class="form-control default-select wide" id="inputState">
+                                                                        <option selected="">Choose...</option>
+                                                                        <option>Option 1</option>
+                                                                        <option>Option 2</option>
+                                                                        <option>Option 3</option>
+                                                                    </select>
+                                                                </div>
+                                                                <div class="mb-3 col-md-2">
+                                                                    <label class="form-label">Zip</label>
+                                                                    <input type="text" class="form-control">
+                                                                </div>
+                                                            </div>
+                                                            <div class="mb-3">
+                                                                <div class="form-check custom-checkbox">
+																	<input type="checkbox" class="form-check-input" id="gridCheck">
+																	<label class="form-check-label form-label" for="gridCheck"> Check me out</label>
+																</div>
+                                                            </div>
+                                                            <button class="btn btn-primary" type="submit">update</button>
+                                                            <button class="btn btn-danger" type="submit">cancel</button>
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+									<!-- Modal -->
+								
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--**********************************
+            Content body end
+        ***********************************-->
+		@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
-
+Route::redirect('/', '/login');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/profile', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('user', App\Http\Controllers\UserController::class);
