bd09a19c0f6504f813bddb5970475b6b4188f2c3
ali ouchagour <ali.ouchagour01@gmail.com>
2023-05-15 21:46:26 +0100

    setup laravel sanctum and create the api v1 for the app (create apiContrller and resources)

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +17,31 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+//protected routes 
+Route::group(['prefix' => 'v1', 'as' => 'api.', 'middleware' => ['auth:sanctum']], function () {
+
+    Route::apiResource('permissions', App\Http\Controllers\Api\V1\Admin\PermissionsApiController::class);
+
+    //Roles
+    Route::apiResource('roles', App\Http\Controllers\Api\V1\Admin\RolesApiController::class);
+
+
+    // Users
+    Route::apiResource('users', App\Http\Controllers\Api\V1\Admin\UsersApiController::class);
+
+
+    // Assets
+    Route::apiResource('assets', App\Http\Controllers\Api\V1\Admin\AssetsApiController::class);
+
+    // Teams
+
+    Route::apiResource('teams', App\Http\Controllers\Api\V1\Admin\TeamApiController::class);
+
+    // Stocks
+    Route::apiResource('stocks', App\Http\Controllers\Api\V1\Admin\StocksApiController::class);
+
+    // Transactions
+    Route::apiResource('transactions', App\Http\Controllers\Api\V1\Admin\TransactionsApiController::class);
+    
 });
